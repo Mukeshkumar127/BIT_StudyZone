07b7245c90294b277cd1fd3a8f784f04f71262e6
Mukesh <anmkumar147@gmail.com>
2025-12-15 18:27:58 +0530

    config updated

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 // const serverUrl = "http://localhost:4001";
-const serverUrl = "https://studyzone-u5nw.onrender.com/"
+const serverUrl = "https://studyzone-u5nw.onrender.com/";
 export default serverUrl;
